03a7715363d63abeba18a81aeacaef4b0b2aae94
virrrex <rex.vishal.3@gmail.com>
2022-02-17 23:08:33 +0530

    initial commit

---
+++ src/test/java/com/smallcase/tracker/PortfolioTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smallcase.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
