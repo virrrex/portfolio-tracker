c47a302de1b3d255d30a0c73fce0efde15a9e033
virrrex <rex.vishal.3@gmail.com>
2022-02-22 12:39:40 +0530

    added constant values

--- src/main/java/com/smallcase/tracker/Constants.java
+++ src/main/java/com/smallcase/tracker/Constants.java
@@ -3,11 +3,12 @@ package com.smallcase.tracker;
 public class Constants {
 	
 	public static final String INVALID_DATA = "Invalid data. Please verify.";
-	public static final String NEGATIVE_QUANTITY = "Final quantity cannot be negative. You don't have enough stocks to sell.";
+	public static final String NEGATIVE_QUANTITY = "Final quantity cannot be negative. You don't have enough shares to sell.";
 	public static final String SELL = "SELL";
 	public static final String BUY = "BUY";
 	public static final String NOT_EXIST = "This trade does not exist. Please verify.";
 	public static final String SYMBOL_NOT_SAME = "Ticker symbol cannot be modified of existing trade. Please add new trade to do so.";
-	
+	public static final String PORTFOLIO_NOT_EXIST = "Portfolio does not exist. You do not have enough shares in your portfolio to remove.";
+	public static final double CURRENT_PRICE = 100.0;
 	
 }
